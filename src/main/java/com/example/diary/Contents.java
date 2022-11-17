d9dc444855dcfe2ab32332723b64a16b0b62c778
pjm6569 <pjm6569@naver.com>
2022-11-17 15:26:07 +0900

    git commit

--- src/main/java/com/example/diary/Contents.java
+++ src/main/java/com/example/diary/Contents.java
@@ -1,48 +1,48 @@
 package com.example.diary;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class Contents extends AppCompatActivity {
+ImageView tocallender;
 
     RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contents);
-
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        ArrayList<CardItem> list = new ArrayList<>();
+        list.add(new CardItem("", ""));
         //리싸이클러 뷰 생성
         recyclerView = findViewById(R.id.recycler_view);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-
-        //리싸이클러뷰 출력
-        recyclerView.setLayoutManager(layoutManager);
-
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
         //어댑터를 통해 리싸이클러 뷰의 타이틀 생성
-        MyRecyclerAdapter adapter = new MyRecyclerAdapter(findViewById(R.id.edit_text));
+        MyRecyclerAdapter adapter = new MyRecyclerAdapter(list);
         recyclerView.setAdapter(adapter);
 
-//        EditText Et = (EditText) findViewById(R.id.ch1);
-//        Et.addTextChangedListener(new TextWatcher() {
-//            @Override
-//            public void beforeTextChanged(CharSequence s,int start,int count,int after) {
-//            }
-//
-//            @Override
-//            public void onTextChanged(CharSequence s,int start,int before,int count) {
-//            }
-//
-//            @Override
-//            public void afterTextChanged(Editable editable) {
-//
-//            }
-//        });
+
+        tocallender=findViewById(R.id.back_button);
+        tocallender.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
 
         //클립보드 이미지 변수 생성
         ImageView img = findViewById(R.id.clip_add_recycler);
@@ -50,30 +50,6 @@ public class Contents extends AppCompatActivity {
         img.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                int currentIndex = adapter.getItems().size()-1;
-
-                try {
-                    String title = getTitleText();
-                    String contents = getContentsText(currentIndex);
-
-                    if (title.isEmpty()) {
-                        Toast.makeText(Contents.this, "어떤 이벤트를 추가하시나요?", Toast.LENGTH_SHORT).show();
-                        return;
-                    }
-                    adapter.setItem(currentIndex,getTitleText(),getContentsText(currentIndex));
-
-                    // 인덱스 0과 "같은제목"의 contents가 비어있는 리싸이클러 뷰 추가
-                    adapter.addItem(getTitleText(),"");
-
-                    //클립보드 버튼을 통한 리싸이클러뷰가 생성 될 때마다 토스트 메세지(디버그용 - 이후에 삭제)
-                    Toast.makeText(Contents.this, "success. count : "+adapter.getItemCount(), Toast.LENGTH_SHORT).show();
-
-                    // 가끔 클립보드 이미지를 여러번 눌렀을때 튕기는 현상을 토스트로 띄움. 앱 강제종료 방지
-                } catch (NullPointerException ex) {
-                    Toast.makeText(Contents.this, "추가에 실패하였습니다", Toast.LENGTH_SHORT).show();
-                    return;
-                }
             }
         });
     }
--- src/main/java/com/example/diary/MyRecyclerAdapter.java
+++ src/main/java/com/example/diary/MyRecyclerAdapter.java
@@ -1,70 +1,84 @@
 package com.example.diary;
 
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.diary.R;
+
 import java.util.ArrayList;
 
-public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.ViewHolder> {
-    private ArrayList<CardItem> arrayList;
-    private EditText editText;
 
-    public MyRecyclerAdapter(EditText editText) {
-        this.arrayList = new ArrayList<>();
-        arrayList.add(new CardItem("",""));
-        this.editText = editText;
+public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.ViewHolder> {
+    private ArrayList<CardItem> mycontents;
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        EditText edt;
+        ImageView IV;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            edt = itemView.findViewById(R.id.rv_content);
+            IV = itemView.findViewById(R.id.insert_image);
+        }
+        public void onBind(CardItem data) {
+        }
+    }
+
+    MyRecyclerAdapter(ArrayList<CardItem> list) {
+        mycontents = list;
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_card, parent, false);
-        return new ViewHolder(view, this.editText);
+        Context context = parent.getContext();
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View view = inflater.inflate(R.layout.item_card, parent, false);
+        ViewHolder vh = new ViewHolder(view);
+
+        return vh;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        CardItem item = arrayList.get(position);
-        holder.title.setText(item.getTitle());
-        holder.contents.setText(item.getContents());
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.onBind(mycontents.get(position));
     }
+
 
     @Override
     public int getItemCount() {
-        return arrayList.size();
+        return mycontents.size();
+
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        protected EditText title;
-        protected EditText contents;
-
-        public ViewHolder(@NonNull View itemView, EditText editText) {
-            super(itemView);
-            this.title = editText;
-            this.contents = itemView.findViewById(R.id.rv_content);
-        }
-    }
-
-    public void addItem(String title, String contents) {
-        arrayList.add(new CardItem(title,contents));
+    public void setItems(ArrayList<CardItem> temp) {
+        mycontents = temp;
         notifyDataSetChanged();
     }
 
-    public void setItem(int currentIndex, String title, String contents) {
-        CardItem beforeItem = arrayList.get(currentIndex);
-        beforeItem.setTitle(title);
-        beforeItem.setContents(contents);
+    public void deleteItems(int position){
+        mycontents.remove(position);
         notifyDataSetChanged();
     }
-//    public void setItems(ArrayList<CardItem> itemList) {
-//        this.arrayList = itemList;
-//        notifyDataSetChanged();
-//    }
 
-    public ArrayList<CardItem> getItems() {
-        return arrayList;
+
+    public void addItems(String title, String image) {
+        mycontents.add(new CardItem(title, image));
+        notifyDataSetChanged();
     }
 }
